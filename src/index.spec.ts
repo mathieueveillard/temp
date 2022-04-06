00d496b24223a8bfb9f1245d943eabdfef390472
Mathieu Eveillard <mathieu.eveillard@gmail.com>
2022-04-06 11:16:30 +0200

    Scope: solution

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,14 +1,20 @@
 test("Can you make this work as expected?", function () {
   const fns = [];
 
-  for (var i = 0; i < 4; i++) {
-    const fn = function () {
-      return i;
+  function createClosure(value: number) {
+    return function (): number {
+      return value;
     };
+  }
+
+  for (var i = 0; i < 4; i++) {
+    const fn = createClosure(i);
     fns.push(fn);
   }
 
-  const actual = fns.map((fn) => fn());
+  expect(fns[0]()).toEqual(0);
+  expect(fns[1]()).toEqual(1);
+  expect(fns[2]()).toEqual(2);
+  expect(fns[3]()).toEqual(3);
   // expect(actual).toEqual([4, 4, 4, 4]); // Actual behavior
-  expect(actual).toEqual([0, 1, 2, 3]); // Expected behavior
 });
