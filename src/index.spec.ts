5237267d0a9a736bd4d8b3841d59bb75dcef04b7
Mathieu Eveillard <mathieu.eveillard@gmail.com>
2022-04-06 12:20:06 +0200

    toPower: solution

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,9 +1,10 @@
-test("Power 2", function () {
-  const actual = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(toPower(2));
-  expect(actual).toEqual([0, 1, 4, 9, 16, 25, 36, 49, 64, 81]);
-});
+const toPower =
+  (valueToRaise: number) =>
+  (power: number): number =>
+    valueToRaise ** power;
 
-test("Power 3", function () {
-  const actual = [0, 1, 2, 3].map(toPower(3));
-  expect(actual).toEqual([0, 1, 8, 27]);
+test("", function () {
+  const valueToRaise = 7;
+  const actual = [0, 1, 2, 3].map(toPower(valueToRaise));
+  expect(actual).toEqual([1, 7, 49, 49 * 7]);
 });
