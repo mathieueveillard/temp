45cf643020dc8a9911103c969c2e5d6919df204e
Mathieu Eveillard <mathieu.eveillard@gmail.com>
2022-04-06 12:07:22 +0200

    Curryfication

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,31 +1,14 @@
-// Ramda, Lodash
-// Partial application: arity 2 => arity 1
-function partial(fn, firstArgument) {
-  return function (i) {
-    return fn(firstArgument, i);
-  };
-}
-
-function partialRight(fn, secondArgument) {
-  return function (i) {
-    return fn(i, secondArgument);
-  };
-}
-
-// arity: 2
-function isMultipleOf(of: number, i: number): boolean {
-  return i % of === 0;
-}
-
-// arity: 1
-// function isMultipleOf3(i: number): boolean {
-//   return isMultipleOf(3, i);
+// Curryfication
+// function isMultipleOf(of: number, i: number): boolean {
+//   return i % of === 0;
 // }
 
+const isMultipleOf =
+  (of: number) =>
+  (i: number): boolean =>
+    i % of === 0;
+
 test("Ok, but…", function () {
-  // const actual = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].filter((i) => isMultipleOf(3, i));
-  // const actual = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].filter((i) => isMultipleOf3(i));
-  const isMultipleOf3 = partial(isMultipleOf, 3);
-  const actual = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].filter(isMultipleOf3);
+  const actual = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].filter(isMultipleOf(3));
   expect(actual).toEqual([0, 3, 6, 9]);
 });
