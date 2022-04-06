4aedb07e36ecf9640d4042dff7b55ebbce4e6571
Mathieu Eveillard <mathieu.eveillard@gmail.com>
2022-04-06 15:49:37 +0200

    Averages: better implementation

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,41 +1,24 @@
-const computeOneAverage = (array: number[]): number => {
-  const sum = array.reduce((acc, cur) => acc + cur, 0);
-  return sum / array.length;
+interface ProtoAverage {
+  sum: number;
+  count: number;
+}
+
+const initialProtoAverage: ProtoAverage = {
+  sum: 0,
+  count: 0,
 };
-
-test("computeOneAverage", function () {
-  expect(computeOneAverage([0, 1, 2])).toEqual(1);
-});
-
-// array =(reduce)=> array up to i =(map: computeOneAverage)=>
-// [0, 1, 2, 3] => [0]          => 0
-//              => [0, 1]       => 0.5
-//              => [0, 1, 2]    => 1
-//              => [0, 1, 2, 3] => 1.5
-
-const computeIntermediaryArrays = (array: number[]): number[][] => {
-  return array.reduce<number[][]>((acc, cur) => {
-    if (acc.length === 0) {
-      return [[cur]];
-    }
-    // acc = [[0], [0, 1]]
-    const lastArray: number[] = acc[acc.length - 1]; // [0, 1]
-    const newArray: number[] = [...lastArray, cur]; // [0, 1, 2]
-    return [...acc, newArray]; // [[0], [0, 1], [0, 1, 2]]
-  }, []);
-};
-
-test("computeIntermediaryArrays", function () {
-  expect(computeIntermediaryArrays([0, 1, 2, 3])).toEqual([
-    [0], //
-    [0, 1],
-    [0, 1, 2],
-    [0, 1, 2, 3],
-  ]);
-});
 
 const computeAverages = (array: number[]): number[] => {
-  return computeIntermediaryArrays(array).map(computeOneAverage);
+  return array
+    .reduce<ProtoAverage[]>((acc, cur) => {
+      const { sum, count } = acc[acc.length - 1] || initialProtoAverage;
+      const protoAverage: ProtoAverage = {
+        sum: sum + cur,
+        count: count + 1,
+      };
+      return [...acc, protoAverage];
+    }, [])
+    .map(({ sum, count }) => sum / count);
 };
 
 test("computeAverages", function () {
