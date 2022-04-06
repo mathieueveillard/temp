f5fe94d8c78434d69fc8a4f4795a92c3d81ccde5
Mathieu Eveillard <mathieu.eveillard@gmail.com>
2022-04-06 12:11:40 +0200

    toPower: problem statement(fix)

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,9 +1,9 @@
 test("Power 2", function () {
-  const actual = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].filter(toPower(2));
+  const actual = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(toPower(2));
   expect(actual).toEqual([0, 1, 4, 9, 16, 25, 36, 49, 64, 81]);
 });
 
 test("Power 3", function () {
-  const actual = [0, 1, 2, 3].filter(toPower(3));
+  const actual = [0, 1, 2, 3].map(toPower(3));
   expect(actual).toEqual([0, 1, 8, 27]);
 });
