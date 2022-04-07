17999cdc21a55acbf09697cd175fea33f93c51f5
Mathieu Eveillard <mathieu.eveillard@gmail.com>
2022-04-07 10:26:42 +0200

    Increment counter

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,10 +1,22 @@
-test("", function () {
-  let wealth = 0;
-  function spareMoney(wealth: number, amount: number): number {
-    return wealth + amount;
-  }
-  expect(spareMoney(0, 1000)).toEqual(1000);
-  expect(spareMoney(500, 1000)).toEqual(1500);
-  wealth = spareMoney(wealth, 1000); // 1000
-  wealth = spareMoney(wealth, 1000); // 2000
+interface State {
+  readonly count: number;
+}
+
+function increment(state: State): State {
+  return {
+    ...state,
+    count: state.count + 1,
+  };
+}
+
+test("Don't do this, const is a false promise!", function () {
+  // GIVEN
+  const state: State = { count: 0 };
+
+  // WHEN
+  const actual = increment(state);
+
+  // THEN
+  expect(state.count).toEqual(0);
+  expect(actual.count).toEqual(1);
 });
