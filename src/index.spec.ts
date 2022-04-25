e8c40ddc4eb5b8200f5db7939621022fd957c83f
Mathieu Eveillard <mathieu.eveillard@gmail.com>
2022-04-25 10:46:04 +0200

    Referential transparency

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,19 @@
-let wealth = 0;
-function spareMoney(wealth: number, amount: number): number {
-  return wealth + amount;
-}
-spareMoney(wealth, 1000);
+test("Actual function call…", function () {
+  function spareMoney(amount: number): number {
+    wealth += amount;
+    return wealth;
+  }
+  let wealth = 1000;
+  const actual = spareMoney(300);
+  expect(actual).toEqual(1300);
+  expect(wealth).toEqual(1300);
+});
+
+test("…replaced by its result", function () {
+  let wealth = 1000;
+  const actual = 1300;
+  expect(actual).toEqual(1300);
+  expect(wealth).toEqual(1000); // Not 1300 anymore!
+});
 
 // Pure = no side cause (scope, XHR, randomness, time) + no side effect (scope, XHR, logging)
