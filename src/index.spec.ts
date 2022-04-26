0363885591cddec9ffab88b9422cd877d798e510
Mathieu Eveillard <mathieu.eveillard@gmail.com>
2022-04-26 15:49:03 +0200

    Idempotence: solution

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,6 +1,7 @@
 namespace Ko {
   test("Can you make spareMoney idempotent?", function () {
     interface AbstractEvent<T extends string, P> {
+      id: string;
       type: T;
       payload: P;
     }
@@ -15,27 +16,38 @@ namespace Ko {
       readonly value: number;
     }
 
+    const wealthReducer =
+      (wealth: Wealth) =>
+      (additionalAmount: number): Wealth => {
+        return {
+          ...wealth,
+          value: wealth.value + additionalAmount,
+        };
+      };
+
+    const pastEvents = new Set<string>();
+
     // A database emulation
     let wealth: Wealth = {
       value: 0,
     };
 
-    function spareMoney({ type, payload: additionalAmount }: Event): void {
-      if (type === "MONEY_SPARED_EVENT") {
-        wealth = {
-          ...wealth,
-          value: wealth.value + additionalAmount,
-        };
+    function spareMoney({ id, type, payload: additionalAmount }: Event): void {
+      if (type !== "MONEY_SPARED_EVENT" || pastEvents.has(id)) {
+        return;
       }
+      wealth = wealthReducer(wealth)(additionalAmount);
+      pastEvents.add(id);
     }
 
     const event: MoneySparedEvent = {
+      id: "b87a6147-9c6e-4cff-bb40-819ccea42c56",
       type: "MONEY_SPARED_EVENT",
       payload: 450,
     };
     spareMoney(event);
     expect(wealth.value).toEqual(450);
     spareMoney(event);
-    expect(wealth.value).toEqual(900);
+    expect(wealth.value).toEqual(450); // Your job: make the test pass!
   });
 }
